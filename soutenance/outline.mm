e7c8f71f0ea6902a3498b92c93ebb619436e926c
Loic Petit <loic.petit@gmail.com>
2012-11-15 16:52:10 +0100

    commit soutenance + abstract dans le manuscrit

---
+++ soutenance/outline.mm
@@ -0,0 +1,290 @@
+<map version="1.0.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1352298491410" ID="ID_1575140205" MODIFIED="1352740448645" STYLE="bubble" TEXT="Soutenance">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1352298508271" ID="ID_1722339060" MODIFIED="1352742741102" POSITION="right" TEXT="Introduction [4]">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1352301108923" ID="ID_1576607260" MODIFIED="1352740584674" TEXT="Cas d&apos;utilisation du r&#xe9;seau local domestique">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1352301127698" ID="ID_300292333" MODIFIED="1352301136672" TEXT="Robert utilise son RLD">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352301267974" ID="ID_1512259570" MODIFIED="1352301280237" TEXT="Le RLD comporte plein d&apos;appareil qui discutent entre eux">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352301137626" ID="ID_100925858" MODIFIED="1352301307596" TEXT="Sa TV rame, il appelle le SAV">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352301149921" ID="ID_1613449834" MODIFIED="1352301258029" TEXT="Le SAV peut pas r&#xe9;pondre car pas de visibilit&#xe9;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352301320412" ID="ID_1430786253" MODIFIED="1352302539101" TEXT="Ce probl&#xe8;me n&apos;est pas rare car...">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1352298511479" ID="ID_232246721" MODIFIED="1352740584676" TEXT="Syst&#xe8;me plus complexes (h&#xe9;t&#xe9;rog&#xe8;nes + distribu&#xe9;s)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1352298524295" ID="ID_1509048561" MODIFIED="1352302545692" TEXT="Capacit&#xe9;s de traitements de donn&#xe9;es plus forte">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352298530831" ID="ID_1907641060" MODIFIED="1352298806386" TEXT="Beaucoup de donn&#xe9;es produites par ces syst&#xe8;mes">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352298536718" ID="ID_718119929" MODIFIED="1352298806386" TEXT="Observer pour mieux comprendre + diagnostic">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352298539222" ID="ID_1037924849" MODIFIED="1352298806387" TEXT="Ref. Fig. 1.1">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1352298550494" ID="ID_1456636261" MODIFIED="1352740584678" TEXT="H&#xe9;t&#xe9;rog&#xe9;n&#xe9;it&#xe9; des donn&#xe9;es du syst&#xe8;me observ&#xe9;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1352298555126" ID="ID_1419162234" MODIFIED="1352298806387" TEXT="Description + int&#xe9;gration">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352298560526" ID="ID_405392222" MODIFIED="1352298806388" TEXT="sch&#xe9;ma: plusieurs syst&#xe8;mes + plusieurs sources diff&#xe9;rentes">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352298565717" ID="ID_69624245" MODIFIED="1352740584794" TEXT="Evolution : Donn&#xe9;es &quot;en mouvement&quot; / &quot;au repos&quot;">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1352298578421" ID="ID_1402958832" MODIFIED="1352298891230" TEXT="Rajouter du mouvement sur le sch&#xe9;ma prec">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1352298584605" ID="ID_229576971" MODIFIED="1352740584795" TEXT="Chaque donn&#xe9;es utilise des moyens d&apos;interrogation diff&#xe9;rent">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1352298588652" ID="ID_1862133402" MODIFIED="1352298806388" TEXT="Interrogation continue vs instantan&#xe9;es"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1352298597717" ID="ID_702186430" MODIFIED="1352740584681" TEXT="Caract&#xe9;ristique de notre syst&#xe8;mes d&apos;observation">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1352385835526" ID="ID_368753119" MODIFIED="1352385918420" TEXT="Langage de manipulation d&#xe9;claratif">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352385921246" ID="ID_32560599" MODIFIED="1352385929033" TEXT="Grande expressivit&#xe9;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352385931892" ID="ID_607984240" MODIFIED="1352385944177" TEXT="Performances = Moins de perturbation">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352385944418" ID="ID_1220838427" MODIFIED="1352740584795" TEXT="Adaptabilit&#xe9; &#xe0; l&apos;application">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1352385966821" ID="ID_512829393" MODIFIED="1352385986054" TEXT="routine sp&#xe9;cifique"/>
+</node>
+<node COLOR="#990000" CREATED="1352385959769" ID="ID_296368711" MODIFIED="1352740584796" TEXT="Adaptabilit&#xe9; &#xe0; l&apos;utilisateur">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1352385986687" ID="ID_241189571" MODIFIED="1352385997913" TEXT="application des perspectives &amp; pr&#xe9;f&#xe9;rences"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1352298606628" ID="ID_683031953" MODIFIED="1352825380251" POSITION="right" TEXT="&#xc9;tat de l&apos;art [8]">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1352298624316" ID="ID_1689354715" MODIFIED="1352825381396" TEXT="Comment faire de l&apos;observation ?">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1352298628971" ID="ID_1757416284" MODIFIED="1352825469904" TEXT="Admin">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1352299784910" ID="ID_1948058971" MODIFIED="1352299785693" TEXT="Capture des donn&#xe9;es"/>
+</node>
+<node COLOR="#990000" CREATED="1352298639163" ID="ID_61892130" MODIFIED="1352825470687" TEXT="Contexte">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1352299791701" ID="ID_1003865896" MODIFIED="1352299798443" TEXT="Interpr&#xe9;tation des donn&#xe9;es"/>
+<node COLOR="#111111" CREATED="1352299798782" ID="ID_879263842" MODIFIED="1352299803452" TEXT="S&#xe9;mantique des donn&#xe9;es"/>
+</node>
+<node COLOR="#990000" CREATED="1352298645668" ID="ID_1685846617" MODIFIED="1352825471495" TEXT="Warehouse, Mining">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1352299813141" ID="ID_296392557" MODIFIED="1352299813819" TEXT="Analyse des masses de donn&#xe9;es"/>
+</node>
+<node COLOR="#990000" CREATED="1352298650387" ID="ID_90026081" MODIFIED="1352825472254" TEXT="Flux, Event">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1352299814845" ID="ID_1985753693" MODIFIED="1352299822812" TEXT="Interrogation en temps r&#xe9;el"/>
+</node>
+<node COLOR="#990000" CREATED="1352298655754" FOLDED="true" ID="ID_231598348" MODIFIED="1352740348618" TEXT="Conclusion : aucun ne r&#xe9;pond &#xe0; l&apos;ensemble de nos exigences">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1352298656946" ID="ID_1336054493" MODIFIED="1352298901798" TEXT="SGFD seul &#xe0; int&#xe9;grer le dynamisme">
+<icon BUILTIN="yes"/>
+</node>
+<node COLOR="#111111" CREATED="1352300922121" ID="ID_1534384084" MODIFIED="1352300934724" TEXT="SGBDR = gestion de catalogue et d&apos;analyse d&apos;historiques">
+<icon BUILTIN="yes"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1352298672514" ID="ID_1433685347" MODIFIED="1352826157424" TEXT="Les SGFD : ce qu&apos;il manque">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1352298679907" ID="ID_1414816042" MODIFIED="1352826159303" TEXT="Formel">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1352299460968" ID="ID_1100069880" MODIFIED="1352299467850" TEXT="Plusieurs mod&#xe8;les existent"/>
+<node COLOR="#111111" CREATED="1352299473493" ID="ID_1594311061" MODIFIED="1352299482471" TEXT="S&#xe9;mantique ambig&#xfc;e sur certains op&#xe9;rateurs"/>
+<node COLOR="#111111" CREATED="1352299482719" ID="ID_1962842696" MODIFIED="1352299538641" TEXT="Liaison forte avec la s&#xe9;mantique d&apos;ex&#xe9;cution"/>
+<node COLOR="#111111" CREATED="1352299558837" ID="ID_153291349" MODIFIED="1352299629399" TEXT="Int&#xe9;gration de plusieurs SGFD difficile">
+<icon BUILTIN="yes"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1352298681666" ID="ID_641363888" MODIFIED="1352826183950" TEXT="Int&#xe9;gration des supports persistants">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1352299361035" ID="ID_313900541" MODIFIED="1352299383721" TEXT="L&apos;int&#xe9;gration est soit implicite (manque de s&#xe9;mantique)"/>
+<node COLOR="#111111" CREATED="1352299384043" ID="ID_876162802" MODIFIED="1352299405767" TEXT="Soit explicite et il faut exprimer deux requ&#xea;tes"/>
+<node COLOR="#111111" CREATED="1352299675361" ID="ID_1816699847" MODIFIED="1352299770066" TEXT="Pas de quoi manipuler les deux sans ambigu&#xef;t&#xe9; s&#xe9;mantique">
+<icon BUILTIN="yes"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1352298687809" ID="ID_457431762" MODIFIED="1352826200605" TEXT="Optimisation de l&apos;&#xe9;valuation de requ&#xea;tes">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1352299415850" ID="ID_1121188045" MODIFIED="1352299432407" TEXT="Optimisations d&apos;op&#xe9;rateurs"/>
+<node COLOR="#111111" CREATED="1352299432673" ID="ID_1890756535" MODIFIED="1352299446847" TEXT="Optimisations de l&apos;&#xe9;valuateur de requ&#xea;tes"/>
+<node COLOR="#111111" CREATED="1352299447377" ID="ID_218622972" MODIFIED="1352300977547" TEXT="Pas d&apos;optimisation globale">
+<icon BUILTIN="yes"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1352299840756" FOLDED="true" ID="ID_420488171" MODIFIED="1352740437774" POSITION="right" TEXT="BIG PICTURE [2]">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1352300526990" ID="ID_1873742801" MODIFIED="1352300822315" TEXT="Mod&#xe9;lisation">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1352300827701" ID="ID_744680628" MODIFIED="1352300830107" TEXT="Ex&#xe9;cution">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1352300830356" ID="ID_1802871969" MODIFIED="1352300832754" TEXT="Persistance">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1352300832987" ID="ID_1068121202" MODIFIED="1352300835979" TEXT="Personnalisation">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1352298698441" ID="ID_293307862" MODIFIED="1352916651057" POSITION="right" TEXT="Le mod&#xe8;le [10]">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1352299182713" ID="ID_624422021" MODIFIED="1352916652987" TEXT="Astral">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1352385742973" ID="ID_1161488747" MODIFIED="1352916708332" TEXT="Exemple">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1352386425664" ID="ID_239334858" MODIFIED="1352386986777" TEXT="IS ( CPU[B] \Join \sigma_{type=video} Applications)"/>
+</node>
+<node COLOR="#990000" CREATED="1352299328452" ID="ID_1608075926" MODIFIED="1352917072698" TEXT="D&#xe9;finition de base">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1352385773308" ID="ID_1041151917" MODIFIED="1352385800465" TEXT="S&#xe9;quence"/>
+<node COLOR="#111111" CREATED="1352385812512" ID="ID_1413133928" MODIFIED="1352385824957" TEXT="Batch"/>
+<node COLOR="#111111" CREATED="1352386230744" ID="ID_1302210336" MODIFIED="1352386236394" TEXT="Relation"/>
+</node>
+<node COLOR="#990000" CREATED="1352387454479" ID="ID_107412773" MODIFIED="1352387458890" TEXT="La s&#xe9;lection">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352387448988" ID="ID_953028794" MODIFIED="1352387450901" TEXT="La jointure">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352386119001" ID="ID_1792044336" MODIFIED="1352387464693" TEXT="Fen&#xea;tre">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352386200681" ID="ID_351884099" MODIFIED="1352386202050" TEXT="Streamers">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352386354822" ID="ID_369477167" MODIFIED="1352386361089" TEXT="Manipulation Temporelle">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1352299185393" ID="ID_1796612629" MODIFIED="1352921178543" TEXT="Ce que l&apos;on gagne">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1352386256869" FOLDED="true" ID="ID_1218198705" MODIFIED="1352740348619" TEXT="Ambigu&#xef;t&#xe9; s&#xe9;mantique">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1352388294998" ID="ID_935845379" MODIFIED="1352388309917" TEXT="S&#xe9;paration mod&#xe8;le/ex&#xe9;cution"/>
+</node>
+<node COLOR="#990000" CREATED="1352387555889" ID="ID_1214857141" MODIFIED="1352387770285" TEXT="Commutativit&#xe9; associativit&#xe9;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352386152577" ID="ID_135223421" MODIFIED="1352387552218" TEXT="Transposabilit&#xe9;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1352298704049" FOLDED="true" ID="ID_1633381661" MODIFIED="1352740434112" POSITION="right" TEXT="La mise en &#x153;uvre [8]">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1352387908824" FOLDED="true" ID="ID_1799403879" MODIFIED="1352740348828" TEXT="Astronef">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1352388530235" ID="ID_1941537878" MODIFIED="1352388553489" TEXT="Choix d&apos;architecture logiciel">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352394391846" FOLDED="true" ID="ID_414464091" MODIFIED="1352740348619" TEXT="Construction d&apos;une requ&#xea;te">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1352394425209" ID="ID_768490640" MODIFIED="1352394426050" TEXT="Fig 6.3"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1352388320522" FOLDED="true" ID="ID_486557382" MODIFIED="1352740348828" TEXT="Asteroid">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1352394430156" ID="ID_453164088" MODIFIED="1352394465156" TEXT="Dynamiques des donn&#xe9;es">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352394465767" ID="ID_730853308" MODIFIED="1352394475918" TEXT="Sch&#xe9;ma physique de persistance">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352394478796" ID="ID_1430371274" MODIFIED="1352394481281" TEXT="Composants">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1352394531944" FOLDED="true" ID="ID_1843882710" MODIFIED="1352740432150" POSITION="right" TEXT="Exp&#xe9;rimentations [6]">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1352394550937" FOLDED="true" ID="ID_1832771150" MODIFIED="1352740348828" TEXT="DomVision">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1352394559148" ID="ID_1705149839" MODIFIED="1352394577006" TEXT="Sch&#xe9;ma du syst&#xe8;me">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352394578117" ID="ID_707036994" MODIFIED="1352394584035" TEXT="Donn&#xe9;es disponibles">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352394584312" ID="ID_579737520" MODIFIED="1352394602699" TEXT="Requ&#xea;tes possibles">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1352394607893" FOLDED="true" ID="ID_955074062" MODIFIED="1352740348829" TEXT="Perfs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1352394624757" ID="ID_691951731" MODIFIED="1352394630184" TEXT="Jointure Asteroid">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1352735110603" ID="ID_1706511303" MODIFIED="1352735116803" TEXT="Mention LinearRoad">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1352394678832" FOLDED="true" ID="ID_1029710623" MODIFIED="1352740456565" POSITION="right" TEXT="Pr&#xe9;f&#xe9;rences [3]">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1352394706334" ID="ID_250163590" MODIFIED="1352740283988" TEXT="Mod&#xe8;le de pr&#xe9;f&#xe9;rence">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1352394712961" ID="ID_1583587159" MODIFIED="1352740283988" TEXT="Int&#xe9;gration">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1352298708897" ID="ID_1783537817" MODIFIED="1352740430198" POSITION="right" TEXT="Conclusion [3]">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
